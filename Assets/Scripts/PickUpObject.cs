f365bb10a2102e94fc314ddef5685508a863ae7d
BANKKII'S <onishield@gmail.com>
2018-02-25 01:13:05 +0700

    Item Panel Test

--- Assets/Scripts/PickUpObject.cs
+++ Assets/Scripts/PickUpObject.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpObject : MonoBehaviour {
+	private bool isPickup = false;
 	public Camera cam;
-
+	public GameObject pickUpPanel;
+	public GameObject itemPanel;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +17,29 @@ public class PickUpObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		RaycastHit hit;
-		if (Physics.Raycast (cam.transform.position, cam.transform.forward,out hit,1f)) {
+		if (Physics.Raycast (cam.transform.position, cam.transform.forward, out hit, 1f)) {
 			print ("HIT");
 			Debug.Log (hit.transform.name);
-			Item target = hit.transform.GetComponent<Item> ();
-			target.Pickup ();
+			if (hit.transform.tag == "Item" && !isPickup ) {
+				pickUpPanel.SetActive (true);
+//			} else {
+//				pickUpPanel.SetActive (false);
+			}
+
+//			Item target = hit.transform.GetComponent<Item> ();
+//			target.Pickup ();
+		} else {
+			pickUpPanel.SetActive (false);
 		}
+	}
+
+	public void IsPiackup(){
+		isPickup = false;
+	}
+		
+	public void PickedUp(){
+		isPickup = true;
+		pickUpPanel.SetActive (false);
+		itemPanel.SetActive (true);
 	}
 }
