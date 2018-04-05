1089c4f58cb88c6d80a12602b7f2ab2efa17b192
BANKKII'S <onishield@gmail.com>
2018-04-05 16:38:30 +0700

    The First Mini Game Adding (Math).
    The First mini game adding, the Math Mini Game, but without a round limit feature.

---
+++ Assets/MiniGame/Scripts/MathBrain.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MathBrain : MonoBehaviour {
+
+	public Text questionText;
+	List<string> operations = new List<string>(new string[] {"add", "subtract", "multiply"});
+	List<int> answerChoices = new List<int>();
+	public GameObject[] buttons = new GameObject[4];
+	int answer = -101;
+
+
+	void Start() 
+	{
+//		buttons = GameObject.FindGameObjectsWithTag("AnswerButton");
+//		Debug.Log ("Buttons length"+buttons.Length);
+//		Question_Generator();
+//		Answer_Generator(answer);
+//		Populate_Answer_Buttons();
+
+	}
+
+	public void MiniGameStart(){
+		Question_Generator ();
+		Answer_Generator (answer);
+		Populate_Answer_Buttons ();
+	}
+	
+	// Generates a random (addition, subtraction, or multiplication) question
+	public void Question_Generator() {
+
+		// Randomly picking an operation from the list
+		string operation = operations[Random.Range(0, 3)]; 
+		// Randomly choose 2 numbers
+		int num1 = Random.Range(1,10);
+		int num2 = Random.Range(1,10);;
+
+		// Based on randomly chosen operation, perform the operation to the 2 numbers
+		switch (operation)
+		{
+			case "add":
+				answer = num1 + num2;
+				Debug.Log(num1 + " + " + num2);
+				questionText.text = num1 + " + " + num2 + " = ?";
+				break;
+			case "subtract":
+				if (num1 > num2) {
+					answer = num1 - num2;
+					Debug.Log(num1 + " - " + num2);
+					questionText.text = num1 + " - " + num2 + " = ?"; 
+				} else {
+					answer = num2 - num1;
+					Debug.Log(num2 + " - " + num1);
+					questionText.text = num2 + " - " + num1 + " = ?"; 
+				}
+				break;
+			case "multiply":
+				answer = num1 * num2;
+				Debug.Log(num1 + " x " + num2);
+				questionText.text = num1 + " x " + num2 + " = ?"; 
+				break;
+			default:
+				Debug.Log("No operation chosen");
+				break;
+		}
+		answerChoices.Add(answer);
+		Debug.Log("Generated Question");
+	}
+
+	// Generate a list of possible answers using the actual answer
+	public void Answer_Generator(int correctAnswer) 
+	{
+		int i = 0;
+		while (i < 3) 
+		{
+			// To randomly choose between addition or subtraction 	
+			if (Random.Range(0,2) == 0) 
+			{
+				// Add a random number to the answer
+				int choice = correctAnswer + Random.Range(1, 5);
+
+				while(answerChoices.Contains(choice))
+				{
+					choice = correctAnswer + Random.Range(1, 5);
+				}
+				answerChoices.Add(choice);
+			} 
+			else
+			{
+				// Subtract a random number from the answer
+				int choice = correctAnswer - Random.Range(1, 5);
+
+				while(answerChoices.Contains(choice))
+				{
+					choice = correctAnswer - Random.Range(1, 5);
+				}
+				answerChoices.Add(choice);
+			}
+
+			i++;
+		}
+		Debug.Log("Generated Answers");
+	}
+
+	// Updates the text of the answer buttons to the answer choices 
+	public void Populate_Answer_Buttons()
+	{
+		//-----adding shuffle algorithm--------
+		int i = 0;
+		foreach (GameObject btn in buttons)
+		{
+			Text btnText = btn.GetComponentInChildren<Text>();
+			btnText.text = answerChoices[i].ToString();
+			i++;
+		}
+		answerChoices.Clear ();
+		Debug.Log("Populated Answer Buttons");
+	}
+
+	public void Check_Answer(Button btn)
+	{
+		if (int.Parse(btn.GetComponentInChildren<Text>().text) == answer) {
+			btn.GetComponent<Image>().color = Color.green;
+			Debug.Log("Correct Answer");
+		} else {
+			btn.GetComponent<Image>().color = Color.red;
+		}
+	}
+}
